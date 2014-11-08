f414cb2324db01a8de16f5e4d1206ad23421d04e
zld <zhu.lida.cn@gmail.com>
2014-11-08 22:54:41 +0800

    init

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by 朱里达 on 14/11/7.
+//  Copyright (c) 2014年 zld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func addName(sender: AnyObject) {
+    }
+
+}
+
